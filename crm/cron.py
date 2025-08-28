ba60f049c0c4a943ef6f6f2b7caeeb95cd0bf4b7
reemelfadilhassanin <justreemjust49@gmail.com>
2025-08-28 15:25:35 +0200

    cron

--- crm/cron.py
+++ crm/cron.py
@@ -4,3 +4,9 @@ def log_crm_heartbeat():
     now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
     with open('/tmp/crm_heartbeat_log.txt', 'a') as f:
         f.write(f"{now} CRM is alive\n")
+import datetime
+
+def log_crm_heartbeat():
+    now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+    with open('/tmp/crm_heartbeat_log.txt', 'a') as f:
+        f.write(f"{now} CRM is alive\n")
--- crm/settings.py
+++ crm/settings.py
@@ -3,3 +3,10 @@ INSTALLED_APPS += ['django_crontab']
 CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
 ]
+INSTALLED_APPS = [
+    # ...
+    'django_crontab',
+]
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
