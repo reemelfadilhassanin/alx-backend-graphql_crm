36d8ab66bd6e500a107823483c34796f58762c87
reemelfadilhassanin <justreemjust49@gmail.com>
2025-08-28 15:16:38 +0200

    crontab

---
+++ crm/cron.py
@@ -0,0 +1,6 @@
+import datetime
+
+def log_crm_heartbeat():
+    now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+    with open('/tmp/crm_heartbeat_log.txt', 'a') as f:
+        f.write(f"{now} CRM is alive\n")
---
+++ crm/settings.py
@@ -0,0 +1,5 @@
+INSTALLED_APPS += ['django_crontab']
+
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
